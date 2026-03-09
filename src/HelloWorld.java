9650753ca9a80bb8c08405f508d5c67cf8031cd6
Carlos,Borja,Javi <carlos@gmail.com>
2026-03-09 10:28:29 +0100

    Añadido método para mostrar la fecha actual

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,7 @@
+import java.time.LocalDate;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hola Mundo!");
+        System.out.println("Fecha actual: " + LocalDate.now());
     }
 }
